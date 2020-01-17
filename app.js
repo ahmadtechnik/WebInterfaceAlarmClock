371aa7140ea9291b4167a34d62862f363339462b
Ahmad's HP 360 Laptop <ahmadkoudier@ah-t.de>
2020-01-17 15:48:50 +0100

    test on linux

--- app.js
+++ app.js
@@ -2,20 +2,70 @@ const express = require('express');
 const jsonfile = require("jsonfile");
 const path = require("path");
 const Omx = require("node-omxplayer");
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
 const app = express();
 const port = 3000;
 var invertals = {};
 
+
+
 //
 app.use(express.static(__dirname + path.join("/", "views")));
 app.use(express.static(__dirname + path.join("/", "node_modules")));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+// GLOBALS 
+var alarmsFilePath = path.join(__dirname, "views", "assets", "alarms.json");
+var mp3FilePath = path.join(__dirname, "views", "assets", "mp3", "Jingle.mp3");
+var exist_file_data = null;
+var player = null;
+var osType = process.platform;
 
-var player = Omx("./views/assets/mp3/Jingle.mp3")
 
+//CALL FUNCTIONS
+invertals["timer_check"] = setInterval(() => {
+    startIntervalAlarms();
+}, 1000);
+/**
+ * handel get main page requst
+ */
 app.get('/', (req, res) => {
     res.sendFile("index.html");
+});
 
+/**
+ * Handel request to add new alarm to list
+ */
+app.post("/addNewTimer", (req, res) => {
+    var passed_data = req.body;
+    var alarm_data = passed_data.alarm_data;
+    var alarm_date = passed_data.alarm_date
+        // write new alarm data to file
+    var exist_file_data = jsonfile.readFileSync(alarmsFilePath);
+    exist_file_data.push({
+        alarm_in: alarm_data,
+        alarm_date: alarm_date,
+        active: 1,
+        repeats: []
+    });
+    jsonfile.writeFileSync(alarmsFilePath, exist_file_data)
+
+    res.send({
+        "data": alarm_data
+    });
+});
+/**
+ * 
+ */
+app.post("/removeAlarm", (req, res) => {
+    var passed_data = req.body;
+    var selected_alarm = jsonfile.readFileSync(alarmsFilePath);
+    selected_alarm[passed_data.index] = "";
+
+    jsonfile.writeFileSync(alarmsFilePath, selected_alarm);
+    res.send("")
 });
 /**
  * 
@@ -24,3 +74,54 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 });
+
+/**
+ * backend prossec
+ */
+var is_running = false;
+/**
+ * 
+ */
+function startIntervalAlarms() {
+    exist_file_data = jsonfile.readFileSync(alarmsFilePath);
+    var h_now = new Date().getHours();
+    var m_now = new Date().getMinutes();
+    var s_now = new Date().getSeconds();
+
+    if (exist_file_data.length > 0) {
+        exist_file_data.forEach(dateObject => {
+            if (typeof dateObject === "object") {
+                var h = new Date(dateObject.alarm_date).getHours();
+                var m = new Date(dateObject.alarm_date).getMinutes();
+                var s = new Date(dateObject.alarm_date).getSeconds();
+                if (!is_running) {
+                    if (h === h_now && m_now === m) {
+                        play_song();
+                        invertals["timer_check"] = setInterval(() => {
+                            startIntervalAlarms();
+                            console.log("Interval set to 60 secound..")
+                        }, 1000 * 60);
+                        is_running = true;
+                    } else {
+                        invertals["timer_check"] = setInterval(() => {
+                            startIntervalAlarms();
+                        }, 1000);
+                        is_running = false;
+                    }
+                }
+            }
+        });
+    }
+}
+
+/**
+ * 
+ * 
+ */
+function play_song() {
+    if (osType === "linux") {
+        player = Omx(mp3FilePath, "both", true);
+    } else {
+        console.log("Not able to start OMX on windows...");
+    }
+}
--- views/main.js
+++ views/main.js
@@ -1,3 +1,95 @@
-(function(params) {
-    console.log("jQquery")
-})($)
+var timePicker = null;
+var time = null;
+$(document).ready(() => {
+    /**
+     * 
+     */
+    timePicker = $(`#timepicker`).timepicker({
+        timeFormat: 'HH:mm:ss',
+        defaultTime: new Date(),
+        dynamic: true,
+        dropdown: false,
+        scrollbar: true,
+        change: (time_) => {
+            time = time_;
+        }
+    });
+
+    /**
+     * 
+     */
+    $(document).on("click", "#add_to_alarms_list", () => {
+        if (time) {
+            var s_hours = new Date(time).getHours();
+            var s_mins = new Date(time).getMinutes();
+            var s_sec = new Date(time).getSeconds();
+            $.ajax("addNewTimer", {
+                async: true,
+                type: "POST",
+                data: {
+                    alarm_data: s_hours + ":" + s_mins + ":" + s_sec,
+                    alarm_date: time
+                },
+                success: (response) => {
+                    console.log(response)
+                }
+            })
+        }
+        update_exist_alarms_table();
+    });
+
+    /**
+     * assign on click of single alarm li 
+     * to remove the alarm 
+     */
+    $(document).on("click", '.alarm_in', (event) => {
+        if (event.ctrlKey) {
+            var current_e = $(event.target).closest("li");
+            var elemetn_index = current_e.attr("id");
+            $.ajax("removeAlarm", {
+                type: "POST",
+                data: {
+                    index: elemetn_index
+                },
+                success: (response) => {
+                    update_exist_alarms_table();
+                }
+            })
+        }
+    });
+
+    update_exist_alarms_table();
+});
+
+
+/**
+ * feach existed alarms 
+ */
+function update_exist_alarms_table() {
+    /**
+     * 
+     */
+    $.getJSON("assets/alarms.json", (data) => {
+        $(`#listOfAlarms`).html("");
+        data.forEach((alarms, i) => {
+            if (typeof alarms === "object") {
+                var is_active = null;
+
+                if (alarms.active === "yes" || alarms.active === 1) {
+                    is_active = "checked";
+                }
+
+                var c_1 = `<label>Active : <input type='checkbox' class='changeAlarmStatus' ${is_active} /></label>`
+                var c_2 = ``;
+                var e_1 = `<div class='alarm_in'>${alarms.alarm_in}</div>`;
+                var e_2 = `<div class='options'>${c_1 }<br>${c_2}</div>`;
+
+                $(`#listOfAlarms`)
+                    .append($(`<li class='singleAlarmLi' id='${i}' >`)
+                        .append(e_1)
+                        .append(e_2)
+                    );
+            }
+        });
+    });
+}
